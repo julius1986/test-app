231ef35e196fe45c2e57fd27af80871a74449b15
julius1986 <uliy86@gmail.com>
2021-02-20 16:33:11 +0200

    add MainApp

---
+++ src/components/MainApp/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import UsersTable from "./../UsersTable"
+export default class MainApp extends Component {
+    render() {
+        return (
+            <div>
+              <UsersTable/>
+            </div>
+        )
+    }
+}
